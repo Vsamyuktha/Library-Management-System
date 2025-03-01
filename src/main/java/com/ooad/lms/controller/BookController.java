e6f15d062a10bc553e0b77a968a4e961a3e7e1ef
Runqing Zhou <zrq166@126.com>
2025-03-01 14:10:33 -0800

    add front end

--- src/main/java/com/ooad/lms/controller/BookController.java
+++ src/main/java/com/ooad/lms/controller/BookController.java
@@ -2,13 +2,12 @@ package com.ooad.lms.controller;
 
 import com.ooad.lms.entity.Book;
 import com.ooad.lms.service.BookService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("/books")
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
 
     private final BookService bookService;
@@ -17,10 +16,8 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    // Search books dynamically
     @GetMapping("/search")
     public List<Book> searchBooks(@RequestParam String field, @RequestParam String value) {
         return bookService.searchBooks(field, value);
     }
-
 }
